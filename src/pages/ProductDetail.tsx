b4c675a2a3ee126c2e84dc3f3b6039537661687d
CertainlyMohneesh <133652369+certainlyMohneeesh@users.noreply.github.com>
2024-12-22 23:57:08 +0530

    forgot to save productdetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Button } from '@/components/ui/button'
-import api from '@/services/api'
+
 
 export default function ProductDetail() {
   const { id } = useParams()
